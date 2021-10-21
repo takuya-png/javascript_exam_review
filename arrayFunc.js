e9703ad7db4e99c4becfe4a8f3c1f7a8d2ff8ba2
takuya nagasawa <tsukasayanagaw474@gmail.com>
2021-10-21 18:20:21 +0900

    配列、ハッシュ、スコープについて

---
+++ arrayFunc.js
@@ -0,0 +1,2 @@
+let teachers = ["noro", "jiro", "nakao"];
+console.log(teachers[1]); 
---
+++ hash.js
@@ -0,0 +1,1 @@
+const playersCount = { baseball: 9,soccer: 11, basketball: 5 };console.log(playersCount["soccer"]);
